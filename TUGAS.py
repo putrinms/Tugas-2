36a06748279943e821210d02362956bebee6cf09
putrinms <80442133+putrinms@users.noreply.github.com>
2021-03-18 09:41:08 -0700

    Add files via upload

---
+++ TUGAS.py
@@ -0,0 +1,17 @@
+def putri():
+    print("\n nama:PUTRI NIRMALASARI \n NIM:TI200039 \n prodi:ti a")
+
+
+putri()
+
+print("jika si a menaiki gedung setinggi 100 m, \n pada siang hari ia naik setinggi 5m, pada malam hari ia merosot 4m, \n maka untuk sampai ke puncak tsb dibtuhkan brp hari??")
+print ("jawab")
+
+
+
+def gedung():
+    print((5-4)*100)
+    print("jadi untuk mencapai puncak tersebut dibuthkan waktu",(5-4)*100), print("hari")
+
+
+gedung()
